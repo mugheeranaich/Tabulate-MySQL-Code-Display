ebef90bb0140be03e65e8755d917626eee7ad1d3
Mugheera <mugheeram567@gmail.com>
2023-06-20 15:11:14 -0400

    Add initial code for retrieving data from MySQL and displaying it in a formatted table

---
+++ data.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from tabulate import tabulate
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="atm"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM atm")
+
+myresult = cursor.fetchall()
+
+data=[]
+for x in myresult:
+    x=list(x)
+    data.append(x)
+table = tabulate(data, headers=["id","Name","username","password","amount"], tablefmt="fancy_grid")
+print(table)
